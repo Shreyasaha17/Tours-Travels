4de13159e39019d19311f4a3b0d87e936994728b
Shreyasaha17 <shreyasaha1704@gmail.com>
2024-07-14 08:24:56 +0530

    commit-message

--- src/data/tourData.js
+++ src/data/tourData.js
@@ -21,55 +21,170 @@ const tourData = [
     },
     {
         id: 2, title: "Bali", image: "/images/bali.jpg",
-        description1: "Visit to Bali will give you a gratifying experience and peace.", price: 72000
+        image1: "https://c0.wallpaperflare.com/preview/557/796/12/pura-ulun-danu-bratan-indonesia.jpg", image2: "https://picstatio.com/large/7e1aa2/coast-sea-landscape-nature.jpg", 
+        image3: "https://c0.wallpaperflare.com/preview/408/841/304/tanah-lot-tanah-lot-temple-entrance-bali.jpg",
+        image4: "https://static.thehoneycombers.com/wp-content/uploads/sites/4/2021/07/Hideout-bali-Beehive-best-villas-hotels-resorts-in-sidemen-east-bali-indonesia-where-to-stay-900x642.jpg",
+        image5: "https://assets.zeezest.com/blogs/PROD_BaliSEO_1643347076064_thumb_1200.jpeg",
+        description1: "Visit to Bali will give you a gratifying experience and peace.", price: 72000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 3, title: "Dubai",
         image: "/images/dubai.jpg",
+        image1: "https://p4.wallpaperbetter.com/wallpaper/141/324/22/hotel-atlantis-dubai-the-palm-wallpaper-preview.jpg", 
+        image2: "https://www.dubaitravelguide.info/wp-content/uploads/burj_khalifa_in_dubai-1.jpg.webp", 
+        image3: "https://media.istockphoto.com/id/1403802469/photo/female-head-decorated-with-flowers-in-miracle-garden-of-dubai.jpg?s=612x612&w=0&k=20&c=t97q60lGQAD6grmmH9yX0SnfbLa0pQphNiIIlzLkOPA=",
+        image4: "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_508380913_20190822131953_20190822132026.jpg",
+        image5: "https://c4.wallpaperflare.com/wallpaper/143/422/428/burj-al-arab-dubai-wallpaper-preview.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 232000
+        price: 232000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 4,
         title: "Maldives",
         image: "/images/maldives.jpg",
+        image1: "https://wallpapercave.com/wp/nNffzXY.jpg", 
+        image2: "https://wallpapercave.com/wp/wp5711644.jpg", 
+        image3: "https://preview.redd.it/m7fwb4orp0f11.jpg?width=640&crop=smart&auto=webp&s=e2e1adcca162c80bba33b19d02227fc4f8890b1b",
+        image4: "https://s1.picswalls.com/wallpapers/2014/07/28/maldives-wallpaper-hd_121018717_122.jpg",
+        image5: "https://e1.pxfuel.com/desktop-wallpaper/413/833/desktop-wallpaper-cool-nature.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 124000
+        price: 124000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 5,
         title: "Kashmir",
         image: "/images/kashmir.jpg",
+        image1: "https://i0.wp.com/www.tusktravel.com/blog/wp-content/uploads/2022/09/Best-Places-to-Visit-Kashmir.jpg?fit=1200%2C798&ssl=1", 
+        image2: "https://img.traveltriangle.com/blog/wp-content/uploads/2020/02/cover-image-of-Valleys-In-Kashmir_18th-jan.jpg", 
+        image3: "https://c1.wallpaperflare.com/preview/659/446/473/landscape-view-himalayas-kashmir.jpg",
+        image4: "https://c4.wallpaperflare.com/wallpaper/542/794/330/brown-leafs-on-forest-forst-cheshire-forst-cheshire-wallpaper-preview.jpg",
+        image5: "https://vargiskhan.com/log/wp-content/uploads/2018/04/gulmarg-in-winter-1-1.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 62000
+        price: 62000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 6,
         title: "Goa",
         image: "/images/goa.jpg",
+        image1: "https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?cs=srgb&dl=pexels-fabianwiktor-994605.jpg&fm=jpg", 
+        image2: "https://www.moneytap.com/blog/wp-content/uploads/2023/04/cost-of-living-in-goa-scaled.jpeg",
+         image3: "https://im.whatshot.in/img/2020/Apr/header-1587650072.jpg",
+         image4: "https://i0.wp.com/www.tusktravel.com/blog/wp-content/uploads/2023/04/Water-Sports-Activities-in-Goa.jpg?fit=1200%2C800&ssl=1",
+         image5: "https://wallpapercave.com/wp/wp3170948.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 52000
+        price: 52000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 7,
         title: "Ladakh",
         image: "/images/ladakh.jpg",
+        image1: "/images/kerala1.jpg", image2: "/images/kerala2.jpg", image3: "/images/kerala3.jpg",image4: "/images/kerala4.jpg",image5: "/images/kerala5.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 730000
+        price: 730000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 8,
         title: "Paris",
         image: "/images/paris.jpg",
+        image1: "/images/kerala1.jpg", image2: "/images/kerala2.jpg", image3: "/images/kerala3.jpg",image4: "/images/kerala4.jpg",image5: "/images/kerala5.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 250000
+        price: 250000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
     {
         id: 9,
         title: "Venice",
         image: "/images/venice.jpg",
+        image1: "/images/kerala1.jpg", image2: "/images/kerala2.jpg", image3: "/images/kerala3.jpg",image4: "/images/kerala4.jpg",image5: "/images/kerala5.jpg",
         description1: "Visit to Kerala will give you a gratifying experience and peace.",
-        price: 265000
+        price: 265000,
+        plan: [
+            "Arrival in Kochi, explore Fort Kochi, Chinese Fishing Nets, and St. Francis Church.",
+            "Drive to Munnar, visit Cheeyappara Waterfalls en route. Explore Munnar town.",
+            "Visit Mattupetty Dam, Echo Point, and Kundala Lake. Explore tea gardens.",
+            "Drive to Thekkady, visit Periyar Wildlife Sanctuary and enjoy a boat ride on Periyar Lake.",
+            "Drive to Alleppey, board a houseboat for an overnight cruise in the backwaters.",
+            "Drive to Kovalam, relax on the beaches of Kovalam and explore local attractions.",
+            "Visit Padmanabhaswamy Temple in Thiruvananthapuram. Return to Kovalam for leisure.",
+            "Departure from Thiruvananthapuram."],
+        time:"20th Aug - 30thAug"
+      
     },
 
 ]
